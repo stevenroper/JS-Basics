f01ac32156f7757f4deb5f3df48060c5519c8f99
Steven Roper <steven.m.roper@gmail.com>
2015-01-21 19:38:37 -0700

    finishing fourth problem.

--- practice.js
+++ practice.js
@@ -68,15 +68,17 @@ var welcome = function() {
 
 //Create a function called myName that returns your name
 
-  //Code Here
+var myName = function() {
+	return 'Steven';
+}
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName();
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName);
 
 //Next problem
 
